641eb8e2540aa3435aed330a17a2708b77f69243
Pablo <tdguchi@gmail.com>
2022-11-18 22:24:17 +0100

    prueba hecha

--- app/Modules/MenuConfiguration/Views/menu_form_modal.php
+++ app/Modules/MenuConfiguration/Views/menu_form_modal.php
@@ -38,7 +38,7 @@
                 </div>
                 <div class="col-12">
                     <div class="mb-3">
-                        <label for="parent">Padre</label>
+                        <label for="parent">Parent</label>
                         <select class="form-control" name="parent" id="parent">
                             <option value=''>Seleccionar Parent</option><? foreach ($s_parent as $c) {
                                                                         ?>
