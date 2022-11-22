92490e70a5764d69bbaf72072a61974ff99901cb
Pablo <tdguchi@gmail.com>
2022-11-22 21:42:17 +0100

    asdsadsa

--- app/Views/partials/menu.php
+++ app/Views/partials/menu.php
@@ -14,7 +14,8 @@ function in_url($link_url)
 <div id="scrollbar">
     <div class="container-fluid">
         <ul class="navbar-nav lateral-menu" id="navbar-nav">
-        <a href="#" onclick="loadModalContent('<?= site_url('tareas/fichar') ?>');" class="btn btn-warning text-dark" data-bs-toggle="modal" data-bs-target="#ajax"><h4>Fichar</h4></a><br>
+        <a href="#" onclick="loadModalContent('<?= site_url('tareas/fichar') ?>');" class="btn btn-warning text-dark" data-bs-toggle="modal" data-bs-target="#ajax"><h4>Fichar</h4
+        ></a><br>
 
             <?php foreach ($menuItems as $item) : ?>
                 <?
