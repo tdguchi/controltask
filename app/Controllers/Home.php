789b9b27104f590f88f0974096300b60e2b4df13
Pablo <tdguchi@gmail.com>
2022-11-22 22:58:10 +0100

    fdsfdsfsdfs

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,27 +4,11 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    function __construct()
-    {
-        $this->ionAuth    = new \IonAuth\Libraries\IonAuth();
-        $this->Asistencias_model =  model('\App\Modules\Asistencias\Models\Asistencias_model');
-    }
-
     public function index()
     {
-        $user_id = $this->ionAuth->user()->row()->id;
-        $data['asistencias'] = $this->Asistencias_model->get_last_asistencia($user_id);
-        if ($data['asistencias'] == null || $data['asistencias']->asistenciatipo_id != 2) {
             $data = array(
-                'main' => 'homepage',
-                'texto_fichar' => 'Fichar entrada'
+                'main' => 'homepage'
             );
-        } else {
-            $data = array(
-                'main' => 'homepage',
-                'texto_fichar' => 'Fichar salida'
-            );
-        }
         return view('template', $data);
     }
 
