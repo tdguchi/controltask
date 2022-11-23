e22767274a795d0f386495c5d9cced123f26ce19
Pablo <tdguchi@gmail.com>
2022-11-23 21:58:51 +0100

    sdsads

--- app/Modules/Asistencias/Views/horario_form.php
+++ app/Modules/Asistencias/Views/horario_form.php
@@ -11,7 +11,17 @@
     <meta name="description" content="<?= isset($description) ? $description : '' ?>">
 
     <script src="<?= base_url() ?>/assets/js/jquery-3.6.0.min.js"></script>
+    <!-- Layout config Js -->
+    <script src="<?= base_url() ?>/assets/js/layout.js"></script>
+    <!-- Bootstrap Css -->
     <link href="<?= base_url() ?>/assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
+    <!-- Icons Css -->
+    <link href="<?= base_url() ?>/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <!-- App Css-->
+    <link href="<?= base_url() ?>/assets/css/app.min.css" id="app-style" rel="stylesheet" type="text/css" />
+    <!-- custom Css-->
+    <link href="<?= base_url() ?>/assets/css/custom.css" id="app-style" rel="stylesheet" type="text/css" />
+    <!-- JAVASCRIPT -->
     <script src="<?= base_url() ?>/assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
 
     <script src="<?= base_url() ?>/assets/libs/select2/select2.min.js"></script>
