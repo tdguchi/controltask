e936790cfba2e5b0f45948efdca9e71793a70cc6
Pablo <tdguchi@gmail.com>
2022-11-23 21:27:01 +0100

    hkjhkjhk

--- app/Modules/Asistencias/Views/horario_form.php
+++ app/Modules/Asistencias/Views/horario_form.php
@@ -2,10 +2,36 @@
 
 <html>
 
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-        <title><?=$titulo?></title>
-    </head>
+<head>
+    <meta charset="utf-8" />
+    <title><?= isset($title) ? $title : 'ControlTask' ?></title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+    <meta name="description" content="<?= isset($description) ? $description : '' ?>">
+
+    <script src="<?= base_url() ?>/assets/js/jquery-3.6.0.min.js"></script>
+    <!-- Layout config Js -->
+    <script src="<?= base_url() ?>/assets/js/layout.js"></script>
+    <!-- Bootstrap Css -->
+    <link href="<?= base_url() ?>/assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css" />
+    <!-- Icons Css -->
+    <link href="<?= base_url() ?>/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <!-- App Css-->
+    <link href="<?= base_url() ?>/assets/css/app.min.css" id="app-style" rel="stylesheet" type="text/css" />
+    <!-- custom Css-->
+    <link href="<?= base_url() ?>/assets/css/custom.css" id="app-style" rel="stylesheet" type="text/css" />
+    <!-- JAVASCRIPT -->
+    <script src="<?= base_url() ?>/assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="<?= base_url() ?>/assets/libs/simplebar/simplebar.min.js"></script>
+    <script src="<?= base_url() ?>/assets/libs/node-waves/waves.min.js"></script>
+    <script src="<?= base_url() ?>/assets/libs/feather-icons/feather.min.js"></script>
+    <script src="<?= base_url() ?>/assets/js/pages/plugins/lord-icon-2.1.0.js"></script>
+    <script src="<?= base_url() ?>/assets/js/plugins.js"></script>
+
+    <script src="<?= base_url() ?>/assets/libs/select2/select2.min.js"></script>
+    <link href="<?= base_url() ?>/assets/libs/select2/select2.min.css" id="app-style" rel="stylesheet" type="text/css" />
+</head>
 
     <body>
     <div class="main-content">
