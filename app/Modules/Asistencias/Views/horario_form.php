88c3ddcb813e83f9995581b72095ae26b0d9e576
Pablo <tdguchi@gmail.com>
2022-11-23 21:14:57 +0100

    jhjhjhjh

--- app/Modules/Asistencias/Views/horario_form.php
+++ app/Modules/Asistencias/Views/horario_form.php
@@ -90,7 +90,7 @@
                                         Cancelar
                                     </a>
                                     <!-- Botón para guardar -->
-                                    <button type="submit" onclick="javascript: $('.add-btn').prop('disabled', true);$('#edit-form').submit();" class="btn btn-green add-btn"><i class="ri-save-line align-bottom ms-2"></i> Guardar</button>
+                                    <button type="submit" class="btn btn-green add-btn"><i class="ri-save-line align-bottom ms-2"></i> Guardar</button>
                                 </div>
                             </div>
                         </div>
