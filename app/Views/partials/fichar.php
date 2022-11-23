25044fe205c50f0637f453d54055eb4a575a8658
Pablo <tdguchi@gmail.com>
2022-11-23 12:10:52 +0100

    sadsadsa

---
+++ app/Views/partials/fichar.php
@@ -0,0 +1,4 @@
+<?php 
+//show button with text fichar
+echo form_button(['type' => 'button', 'class' => 'btn btn-warning text-dark', 'content' => '<h4>Fichar</h4>']);
+?>
--- app/Views/template.php
+++ app/Views/template.php
@@ -58,6 +58,9 @@
         <?= view('partials/menu'); ?>
     </div>
     <div id="layout-wrapper">
+        <?= view('partials/fichar'); ?>
+    </div>
+    <div id="layout-wrapper">
         <?= view($main); ?>
     </div>
 
