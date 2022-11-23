73b86fc92f6c7a622a6efd10259cc14aa3a3892e
Pablo <tdguchi@gmail.com>
2022-11-23 12:11:59 +0100

    agsadgsajdgaj

--- app/Views/partials/fichar.php
+++ app/Views/partials/fichar.php
@@ -1,4 +1,3 @@
 <?php 
-//show button with text fichar
-echo form_button(['type' => 'button', 'class' => 'btn btn-warning text-dark', 'content' => '<h4>Fichar</h4>']);
+echo '<button>Click me</button>';
 ?>
