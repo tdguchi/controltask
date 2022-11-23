e373eb6a547ac2d9b1e9e7afc6dfc77a83c977a1
Pablo <tdguchi@gmail.com>
2022-11-23 15:55:35 +0100

    khkjhkjhjh

--- app/Modules/Asistencias/Views/fichar.php
+++
@@ -1,6 +0,0 @@
-<?php 
-echo form_open('tareas/fichar');
-echo form_submit('fichar', 'Fichar');
-echo form_submit('desfichar', 'Desfichar');
-echo form_close();
-?>
--- app/Views/partials/fichar.php
+++
@@ -1,3 +0,0 @@
-<?php 
-echo '<button>Click me</button>';
-?>
