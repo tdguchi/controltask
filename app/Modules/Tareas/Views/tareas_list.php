a05c80293a32fbfc62125d0b6c51cb68634a563a
Pablo <tdguchi@gmail.com>
2022-11-23 19:34:38 +0100

    asdsadsa

--- app/Modules/Tareas/Views/tareas_list.php
+++ app/Modules/Tareas/Views/tareas_list.php
@@ -16,7 +16,7 @@
                             <div class="d-flex align-items-center">
                                 <h5 class="card-title mb-0 flex-grow-1 h5-title text-capitalize"><?= $titulo ?> <?= $element ?></h5>
                                 <div class="flex-shrink-0">
-                                    <?php if ($group_id == 1) { ?>
+                                <?php  if (count($group_id) == 2) {?>
                                         <span class="text-capitalize"><?php echo anchor(site_url('tareas/view/0/1'), 'Propias', 'class="btn btn-green add-btn"'); ?></span>
                                         <span class="text-capitalize"><?php echo anchor(site_url('tareas/view/0/2'), 'Todas', 'class="btn btn-green add-btn"'); ?></span>
                                     <?php } ?>
