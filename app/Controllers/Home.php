107f6e316dc50b6a62e3c42e015c9a9c268ee72a
Pablo <tdguchi@gmail.com>
2022-11-24 21:27:27 +0100

    asdsadas

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -7,7 +7,7 @@ class Home extends BaseController
     public function index()
     {
             $data = array(
-                'main' => 'homepage'
+                'main' => 'App\Modules\Asistencias\Views\asistencias_form'
             );
         return view('template', $data);
     }
