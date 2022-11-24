24f248dbb7881630de17f8e0fd2040ddf6de4ca4
Pablo <tdguchi@gmail.com>
2022-11-24 16:23:00 +0100

    asdsadsa

--- app/Modules/Tareas/Views/tareas_list.php
+++ app/Modules/Tareas/Views/tareas_list.php
@@ -105,7 +105,6 @@
                                                     <td class=" text-right "><?= $row->horasreales ?></td>
                                                     <td class=" text-right "><?= $row->texto_estado ?></td>
                                                     <td class=" text-right ">
-                                                       <? var_dump($quien) ?>
                                                         <? if ($quien === '1') { ?>
                                                             <?= form_open('tareas/acciones/' . $row->tarea_id, 'id="actionForm' . $row->tarea_id . '"') ?>
                                                             <? if ($row->estado == 0) { ?>
