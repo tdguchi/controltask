1bef1da2debac3601bed5629eab885c310f8f51b
Pablo <tdguchi@gmail.com>
2022-11-24 22:11:46 +0100

    asdsadsa

--- app/Modules/Asistencias/Views/asistencias_list.php
+++ app/Modules/Asistencias/Views/asistencias_list.php
@@ -60,9 +60,9 @@
                                                     <td class=" text-right "><?= $row->nombre ?></td>
                                                     <td class=" text-right ">
                                                     <? if ($row->asistenciatipo_id == 0) { ?>
-                                                        <span style="color:green" ><i class='bx bx-exit bx-rotate-180 fs-22' ></i> <?= $row->tipo?> </span>
+                                                        <span style="color:green" ><i class='bx bx-exit bx-rotate-180' ></i> <?= $row->tipo?> </span>
                                                     <? } else { ?>
-                                                        <span style="color:green" ><i style="color:red" class='bx bx-exit fs-22' ></i> <?= $row->tipo?> </span>
+                                                        <span style="color:green" ><i style="color:red" class='bx bx-exit' ></i> <?= $row->tipo?> </span>
                                                     <? } ?>
                                                 </td>
                                                     <td class=" text-left "><?= date("d/m/Y H:i:s", strtotime($row->fechahora)) ?></td>
