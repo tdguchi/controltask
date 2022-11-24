d58b02ade3c5c68faa6c8fa60c55a3114823741f
Pablo <tdguchi@gmail.com>
2022-11-24 13:54:56 +0100

    asdsadsa

--- app/Modules/Tareas/Views/tareas_list.php
+++ app/Modules/Tareas/Views/tareas_list.php
@@ -107,11 +107,11 @@
                                                     <td class=" text-right ">
                                                         <?= form_open('tareas/acciones/' . $row->tarea_id, 'id="actionForm' . $row->tarea_id . '"') ?>
                                                         <? if ($row->estado == 0) { ?>
-                                                            <button type="submit" id="<?=$row->tarea_id?>" title="Iniciar tarea" class="btn" value="0"><i class="bx bx-play fs-22"></i></button>
+                                                            <button type="submit" id="0" title="Iniciar tarea" class="btn" value="0"><i class="bx bx-play fs-22"></i></button>
                                                         <? } else if ($row->estado == 1) { ?>
-                                                            <button type="submit" id="<?=$row->tarea_id?>" title="Pausar tarea" class="btn" value="1"><i class="bx bx-pause fs-22"></i></button>
+                                                            <button type="submit" id="1" title="Pausar tarea" class="btn" value="1"><i class="bx bx-pause fs-22"></i></button>
                                                         <? } ?>
-                                                        <button type="submit" id="<?=$row->tarea_id?>" title="Tarea acabada" class="btn"><i class="ri-calendar-check-line"></i></button>  
+                                                        <button type="submit" id="2" title="Tarea acabada" class="btn"><i class="ri-calendar-check-line"></i></button>  
                                                         <?= form_close() ?>
                                                     </td>
                                                 </tr>
