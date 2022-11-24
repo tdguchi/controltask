22e65ebb2977fe36c8a0326cd884afe8e51d6d7c
Pablo <tdguchi@gmail.com>
2022-11-24 21:28:46 +0100

    asdsadsa

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,8 +6,20 @@ class Home extends BaseController
 {
     public function index()
     {
-            $data = array(
-                'main' => 'App\Modules\Asistencias\Views\asistencias_form'
+        $data = array(
+            'button' => 'Añadir',
+            'fun' => 'create',
+            'action' => site_url('asistencias/fichar_action') . ($this->request->getGet('from') ? ('/' . urlencode($this->request->getGet('from'))) : ''),
+            'from' => $this->request->getGet('from') ? $this->request->getGet('from') : NULL,
+            'data_fields' => array(
+                'asistencia_id' => "",
+                'fechahora' => "",
+                'fechahora_timestamp' => "",
+                'asistenciatipo_id' => "",
+                'usuario_id' => "",
+                'comentario' => ""
+            ),
+            'main' => 'App\Modules\Asistencias\Views\asistencias_form'
             );
         return view('template', $data);
     }
