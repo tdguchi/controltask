2d93b1aa6e31c878f2d5be8b9d07c1c336dcc423
Pablo <tdguchi@gmail.com>
2022-11-24 22:25:35 +0100

    adsadsada

--- app/Modules/Asistencias/Views/asistencias_list.php
+++ app/Modules/Asistencias/Views/asistencias_list.php
@@ -15,7 +15,9 @@
                         <div class="card-header border-0">
                             <div class="d-flex align-items-center">
                                 <h5 class="card-title mb-0 flex-grow-1 h5-title text-capitalize"><?= $titulo ?> <?= $element ?></h5>
-                                <input id="startDate" class="form-control" type="date" />
+                                <div class="flex-shrink-0">
+                                    <input id="startDate" class="form-control" type="date" />
+                                </div>
                                 <div class="flex-shrink-0">
                                     <?php if (count($group_id) == 2) { ?>
                                         <span class="text-capitalize"><?php echo anchor(site_url('asistencias/view/0/1'), 'Propias', 'class="btn btn-green add-btn"'); ?></span>
