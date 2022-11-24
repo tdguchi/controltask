629d5457906a89e56f3ee74f4b64d98f45823627
Pablo <tdguchi@gmail.com>
2022-11-24 22:03:15 +0100

    asdsada

--- app/Modules/Asistencias/Views/asistencias_list.php
+++ app/Modules/Asistencias/Views/asistencias_list.php
@@ -58,12 +58,13 @@
                                                         </div>
                                                     </th>
                                                     <td class=" text-right "><?= $row->nombre ?></td>
-                                                    <td class=" text-right "><?= $row->tipo?>
+                                                    <td class=" text-right ">
                                                     <? if ($row->asistenciatipo_id == 0) { ?>
-                                                        <i class='bx bx-door-open'></i>
+                                                        <i style="color:green" class='bx bx-door-open'></i>
                                                     <? } else { ?>
-                                                        <i class='bx bx-exit' ></i>
+                                                        <i style="color:red" class='bx bx-exit' ></i>
                                                     <? } ?>
+                                                    <?= $row->tipo?>
                                                 </td>
                                                     <td class=" text-left "><?= date("d/m/Y H:i:s", strtotime($row->fechahora)) ?></td>
                                                     <td class=" text-left "><?= $row->comentario ?></td>
