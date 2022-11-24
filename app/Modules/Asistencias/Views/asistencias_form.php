d0a0ca100f7936889e550e8e12ac2828a344aa1d
Pablo <tdguchi@gmail.com>
2022-11-24 21:46:09 +0100

    asdsada

--- app/Modules/Asistencias/Views/asistencias_form.php
+++ app/Modules/Asistencias/Views/asistencias_form.php
@@ -18,7 +18,7 @@
                                             <div class="row">
                                                     <div class="col-12">
                                                         <div class="mb-12">
-                                                            <h1 class="h1-title">Necesitas registrar una entrada para poder usar la aplicación</h1>
+                                                            <h2 class="h1-title">Necesitas registrar una entrada para poder usar la aplicación</h2>
                                                         </div>
                                                     </div>
                                                     <input type="hidden" name="asistencia_id" value="<?php echo $data_fields['asistencia_id']; ?>" />
