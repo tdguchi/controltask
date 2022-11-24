7c8e5929da7ce374b544ab5c635f428e4a83667f
Pablo <tdguchi@gmail.com>
2022-11-24 22:19:19 +0100

    asdsada

--- app/Modules/Asistencias/Views/asistencias_list.php
+++ app/Modules/Asistencias/Views/asistencias_list.php
@@ -14,7 +14,7 @@
                     <div class="card card-list-soaga">
                         <div class="card-header border-0">
                             <div class="d-flex align-items-center">
-                                <h5 class="card-title mb-0 flex-grow-1 h5-title text-capitalize"><?= $titulo ?> <?= $element ?></h5>
+                                <h5 class="card-title mb-12 flex-grow-1 h5-title text-capitalize"><?= $titulo ?> <?= $element ?></h5>
                                 <div class="flex-shrink-0">
                                     <?php if (count($group_id) == 2) { ?>
                                         <span class="text-capitalize"><?php echo anchor(site_url('asistencias/view/0/1'), 'Propias', 'class="btn btn-green add-btn"'); ?></span>
