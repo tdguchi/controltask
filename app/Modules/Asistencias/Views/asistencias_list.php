f5348e7dae8369ca4231a90ab5eaf9a55a2421d2
Pablo <tdguchi@gmail.com>
2022-11-25 16:48:02 +0100

    asdad

--- app/Modules/Asistencias/Views/asistencias_list.php
+++ app/Modules/Asistencias/Views/asistencias_list.php
@@ -1,9 +1,10 @@
 <div class="main-content">
     <div class="page-content">
         <div class="container-fluid">
-            <div class="row">
+        <div class="row">
                 <div class="col text-center">
-                    <a href="<?= site_url("asistencias/fichar") ?>" class="btn btn-warning btn-lg text-dark">Fichar</a>
+                    <h4>Actualmente estás  <?= $fichado ? "dentro del sistema" : "fuera del sistema" ?></h4>
+                    <a href="#" onclick="loadModalContent('<?= site_url('asistencias/fichar/1') ?>');" data-bs-toggle="modal" data-bs-target="#ajax" class="btn btn-warning btn-lg text-dark">Fichar  <?= $fichado ? "Salida" : "Entrada" ?></a>
                 </div>
             </div><br>
             <?php if (isset($message)) : ?>
