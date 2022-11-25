89355ad81bb2622d3a3f5690e483d214056e6977
Pablo <tdguchi@gmail.com>
2022-11-25 09:26:15 +0100

    asdasd

--- app/Modules/Asistencias/Controllers/Asistencias.php
+++ app/Modules/Asistencias/Controllers/Asistencias.php
@@ -208,7 +208,8 @@ class Asistencias extends BaseController
             $fichado = true;
         }
         $jornada = $this->Asistencias_model->get_jornada($p, $user_id);
-        if ($jornada != null or count($jornada) == 2) {
+        log_message("error", "JORNADA: " . print_r($jornada,true));
+        if ($jornada != null || count($jornada) == 2) {
             $totalhoras = $jornada[1]->total - $jornada[0]->total;
         } else {
             $totalhoras = 0;
