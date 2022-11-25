6a56ef169d2dd4d41e3010ac7551ceb7b92aa4af
Pablo <tdguchi@gmail.com>
2022-11-25 12:15:59 +0100

    sadsad

--- app/Modules/Asistencias/Controllers/Asistencias.php
+++ app/Modules/Asistencias/Controllers/Asistencias.php
@@ -242,6 +242,8 @@ class Asistencias extends BaseController
             $accion = site_url('asistencias/view/0/1');
         } else if ($modal != null && $quien != null && $q != null) {
             $accion = site_url('asistencias/view/' . $modal . '/' . $quien);
+        } else {
+            $accion = site_url('asistencias/view');
         }
         $data = array(
             'accion' =>  $accion,
