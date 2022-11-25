90f6d798c5ccde9f0f813e6a87f796b5f5e9f1b2
Pablo <tdguchi@gmail.com>
2022-11-25 12:30:06 +0100

    sadsa

--- app/Modules/Asistencias/Controllers/Asistencias.php
+++ app/Modules/Asistencias/Controllers/Asistencias.php
@@ -236,9 +236,9 @@ class Asistencias extends BaseController
         } else {
             $totalhoras = 0;
         }
-        if ($modal == false && $quien == null && $q == null) {
+        if ($modal == false && $quien == null && $q != null) {
             $accion = site_url('asistencias/view/0/1');
-        } else if ($modal != false && $quien == null && $q == null) {
+        } else if ($modal != false && $quien == null && $q != null) {
             $accion = site_url('asistencias/view/0/1');
         } else if ($modal != false && $quien != null) {
             $accion = site_url('asistencias/view/' . $modal . '/' . $quien);
