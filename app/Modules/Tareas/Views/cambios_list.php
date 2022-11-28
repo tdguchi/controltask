6dbe060f003ba006959309442ac02c64c2a18b76
Pablo <tdguchi@gmail.com>
2022-11-28 17:59:42 +0100

    asdsadsa

--- app/Modules/Tareas/Views/cambios_list.php
+++ app/Modules/Tareas/Views/cambios_list.php
@@ -46,7 +46,7 @@
                                                     </td>
                                                     <td class=" text-right "><?= $row->usuario_id ?></td>
                                                     <td class=" text-left "><?= date("d/m/Y", strtotime($row->fechahora)) ?></td>
-                                                    <td class=" text-left "><?= date("d/m/Y", strtotime($row->cambiado)) ?></td>
+                                                    <td class=" text-left "><?= date("d/m/Y", strtotime($row->cambiados)) ?></td>
                                                 </tr>
                                             <? } ?>
                                         </tbody>
