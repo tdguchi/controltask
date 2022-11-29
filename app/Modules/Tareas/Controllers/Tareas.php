3423c0e2fbed7ca206095d94af670b525dc05ad7
Pablo <tdguchi@gmail.com>
2022-11-29 11:03:14 +0100

    asdsads

--- app/Modules/Tareas/Controllers/Tareas.php
+++ app/Modules/Tareas/Controllers/Tareas.php
@@ -426,15 +426,12 @@ class Tareas extends BaseController
             $this->eventAfterUpdate($this->request->getPost('tarea_id'));
             $row2 = (array) $this->Tareas_model->get_by_id($id);
             $diff = array_diff($row2,$row);
-            //get array with old and new values for each field with differences on two arrays given
-            $diff = array_map(function($a, $b) {
-                return array('old' => $a, 'new' => $b);
-            }, $row, $row2);
-            log_message('error', 'Tarea modificada: ' . json_encode($diff));
+            $diff2 = array_diff($row,$row2);
             $datalog = array (
                 'tarea_id' => $id,
                 'usuario_id' => $this->ionAuth->user()->row()->id,
                 'fechahora' => date('Y-m-d H:i:s'),
+                'originales' => json_encode($diff2),
                 'cambiados' => json_encode($diff)
             );
 
