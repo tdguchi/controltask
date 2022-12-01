ed5e177ffb3a1f9d1a498479caa27a14f3dc649f
Pablo <tdguchi@gmail.com>
2022-12-01 09:52:26 +0100

    asdsadasdsa

--- app/Views/partials/fichar.php
+++ app/Views/partials/fichar.php
@@ -2,6 +2,6 @@
                 <div class="col text-center">
                     <h4>Actualmente estás  <?= $fichado ? "dentro del sistema" : "fuera del sistema" ?></h4>
                     <h4>Es necesario estar dentro del sistema para interactuar con él</h4>
-                    <a href="#" onclick="loadModalContent('<?= site_url('asistencias/fichar/1') ?>');" data-bs-toggle="modal" data-bs-target="#ajax" class="btn <?= $fichado ? 'btn-warning' : 'btn-success' ?> btn-lg text-dark">Fichar  <?= $fichado ? "Salida" : "Entrada" ?></a>
+                    <a href="#" onclick="loadModalContent('<?= site_url('asistencias/fichar/1') ?>');" data-bs-toggle="modal" data-bs-target="#ajax" class="btn <?= $fichado ? 'btn-danger' : 'btn-success' ?> btn-lg text-dark">Fichar  <?= $fichado ? "Salida" : "Entrada" ?></a>
                 </div>
             </div><br>
