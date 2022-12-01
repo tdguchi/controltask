df88481031842efd137290c85ae3a90a628d48aa
Pablo <tdguchi@gmail.com>
2022-12-01 10:44:32 +0100

    accesibilidad v7

--- app/Modules/Asistencias/Views/asistencias_list.php
+++ app/Modules/Asistencias/Views/asistencias_list.php
@@ -13,22 +13,22 @@
                                 <h5 class="card-title mb-0 flex-grow-1 h5-title"><?= $titulo ?> <?= $element ?> <?= $q ? ' filtradas por: ' . $q : (' del ' . $p != null ? date('d-m-Y', strtotime($p)) : date('d-m-Y')) ?></h5>
                                 <div class="flex-shrink-0">
                                     <form id="date-box" class="input-group" action="<?= $accion ?>" method="post">
-                                        <input class="form-control" id="p" name="p" type="date" value="<?= $p != null ? $p : date('Y-m-d') ?>" />
-                                        <button type="submit" class="btn btn-outline-dark"><i class="ri-search-line search-icon"></i></button>
+                                        <input title="Campo buscar fecha" class="form-control" id="p" name="p" type="date" value="<?= $p != null ? $p : date('Y-m-d') ?>" />
+                                        <button title="Boton buscar fecha" type="submit" class="btn btn-outline-dark"><i class="ri-search-line search-icon"></i></button>
                                     </form>
                                 </div>
                                 <div class="flex-shrink-0">
                                     <?php if (count($group_id) == 2) { ?>
-                                        <span class="text-capitalize"><?php echo anchor(site_url('asistencias/view/0/1'), 'Propias', 'class="btn btn-green add-btn"'); ?></span>
-                                        <span class="text-capitalize"><?php echo anchor(site_url('asistencias/view/0/2'), 'Todas', 'class="btn btn-green add-btn"'); ?></span>
+                                        <span title="Listar asistencias propias" class="text-capitalize"><?php echo anchor(site_url('asistencias/view/0/1'), 'Propias', 'class="btn btn-green add-btn"'); ?></span>
+                                        <span title="Listar todas asistencias" class="text-capitalize"><?php echo anchor(site_url('asistencias/view/0/2'), 'Todas', 'class="btn btn-green add-btn"'); ?></span>
                                     <?php } ?>
                                     <div class="search-box-table ms-2">
                                         <form id="search-box" class="input-group" action="<?= $accion ?>" method="post">
                                             <input type="hidden" name="filter" value="<?= $filter == "" ? "" : explode("=", $filter)[1] ?>">
                                             <input type="hidden" name="title" value="<?= $custom_title == "" ? "" : explode("=", $custom_title)[1] ?>">
-                                            <input type="text" class="form-control search-c border-black" placeholder="Buscar..." id="q" name="q" value="<?= $q ?>">
-                                            <button type="button" onclick="resetSearch();" class="btn btn-ghost-dark waves-effect waves-light"><i class="ri-close-line "></i></button>
-                                            <button type="submit" class="btn btn-outline-dark"><i class="ri-search-line search-icon"></i></button>
+                                            <input title="Campo buscar" type="text" class="form-control search-c border-black" placeholder="Buscar..." id="q" name="q" value="<?= $q ?>">
+                                            <button title="Resetear campo buscar" type="button" onclick="resetSearch();" class="btn btn-ghost-dark waves-effect waves-light"><i class="ri-close-line "></i></button>
+                                            <button title="Boton buscar" type="submit" class="btn btn-outline-dark"><i class="ri-search-line search-icon"></i></button>
                                         </form>
                                     </div>
                                 </div>
