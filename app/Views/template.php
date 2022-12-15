0ce3a8d7bf130c943559cd91f5255767539d294a
Pablo <tdguchi@gmail.com>
2022-12-15 21:40:21 +0100

    sdadas

--- app/Views/template.php
+++ app/Views/template.php
@@ -65,7 +65,11 @@
     <div id="layout-wrapper">
         <?= view($main); ?>
     </div>
-
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+    integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+    crossorigin="anonymous"></script>
     <script src="<?= base_url() ?>/assets/libs/apexcharts/apexcharts.min.js"></script>
     <script src="<?= base_url() ?>/assets/js/pages/dashboard-crm.init.js"></script>
     <script src="<?= base_url() ?>/assets/libs/list.js/list.min.js"></script>
